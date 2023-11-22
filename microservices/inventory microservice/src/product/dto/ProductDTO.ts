a8605014c3dccad11559d9b964c2ded69484c805
chiheb ben cheikh <100644963+ChihebBENCHEIKH1@users.noreply.github.com>
2023-11-22 21:44:38 +0100

    Delete microservices directory

--- microservices/inventory microservice/src/product/dto/ProductDTO.ts
+++
@@ -1,7 +0,0 @@
-export interface ProductDTO {
-    name?: string;
-    description?: string;
-    price?: number;
-    inventory_id: string;
-    availability_status: string;
-  }
--- microservices/inventory microservice/src/product/service/IProductService.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from  "../entity/Product";
-
-export interface IProductService {
-  getAllProducts(): Promise<IProduct[]>;
-  getProductById(id: string): Promise<IProduct|null>;
-  createProduct(productData: IProduct): Promise<IProduct>;
-  updateProduct(id: string, productData: IProduct): Promise<IProduct | null>;
-  deleteProduct(id: string): Promise<boolean>;
-}
